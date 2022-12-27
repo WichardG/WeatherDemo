d78d82cd635392c68f9ba9d72315002395a45b9c
Wichard Gerrits <wichardgh@gmail.com>
2022-12-27 14:29:04 +0100

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Wichard Gerrits on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
